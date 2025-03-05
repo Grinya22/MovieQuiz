d18f0865c13f9855fe6148938b1429240ede3c0b
Grinya <gregory.vanyurin@icloud.com>
2025-03-05 16:57:14 +0400

    finished MVP and Tests

---
+++ MovieQuizTests1/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests1
+//
+//  Created by Gregory Vanyurin on 26.02.2025.
+//
+
+import Foundation
---
+++ MovieQuizTests1/MovieQuizTestsPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizTestsPresenter.swift
+//  MovieQuizTests1
+//
+//  Created by Gregory Vanyurin on 24.02.2025.
+//
+
+import Foundation
